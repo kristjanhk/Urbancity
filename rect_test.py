f49ff15745f703d9f6a7ce182860bb9735586c95
Kristjan Hendrik Küngas <kristjankyngas@outlook.com>
2016-01-26 23:57:27 +0200

    sdl2 debug link

--- rect_test.py
+++ rect_test.py
@@ -1,3 +1,5 @@
+# https://github.com/renpy/pygame_sdl2/issues/31
+
 # Comment out these two lines to go back to old Pygame version
 import pygame_sdl2
 pygame_sdl2.import_as_pygame()
@@ -7,32 +9,39 @@ from pygame import QUIT
 import sys
 
 
-class TestObj(pygame.sprite.DirtySprite):
+class WhiteSquare(pygame.sprite.DirtySprite):
     def __init__(self):
         pygame.sprite.DirtySprite.__init__(self)
         self.dirty = 2
         # noinspection PyArgumentList
         self.image = pygame.Surface((10, 10))
         self.image.fill((255, 255, 255))
-        self.x = 0
         self.rect = self.image.get_rect()
 
     def update(self):
-        self.x += 1
-        self.rect.topleft = (self.x, 0)
+        self.rect.x += 1
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((500, 50))
     clock = pygame.time.Clock()
+
     allsprites = pygame.sprite.LayeredDirty()
-    allsprites.add(TestObj())
+    allsprites.add(WhiteSquare())
+
+    # noinspection PyArgumentList
+    background = pygame.Surface((500, 50))
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+    allsprites.clear(screen, background)
+
     while True:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
+
         allsprites.update()
         dirtyrects = allsprites.draw(screen)
         pygame.display.update(dirtyrects)
